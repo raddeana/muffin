f32075dc6c324776c98fec16579937203ee5b90c
mulberry <wowcxy2008@126.com>
2018-01-31 22:13:28 +0800

    Update swap.c

--- c/algorithm/basic/sort/sort/swap.c
+++ c/algorithm/basic/sort/sort/swap.c
@@ -2,16 +2,16 @@
  * 交换
  * @author mulberry
  */
-void swap (int &ele1, int &ele2);
+void swap (int *ele1, int *ele2);
 
 /**
  * 数组元素交换位置
  * @param { int ref } 元素指针1
  * @param { int ref } 元素指针2
  */
-void swap (int &ele1, int &ele2) {
+void swap (int *ele1, int *ele2) {
   int temp;
-  temp = ele1;
-  ele1 = ele2;
-  ele2 = temp;
+  temp = *ele1;
+  *ele1 = *ele2;
+  *ele2 = temp;
 } 
