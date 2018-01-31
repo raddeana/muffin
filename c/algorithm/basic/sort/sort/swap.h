0ed7ceb9ab4b316281f08f20ebeba7d187e798c5
mulberry <wowcxy2008@126.com>
2018-01-31 22:20:55 +0800

    Update swap.h

--- c/algorithm/basic/sort/sort/swap.h
+++ c/algorithm/basic/sort/sort/swap.h
@@ -1,1 +1,17 @@
+/**
+ * 交换
+ * @author mulberry
+ */
+void swap (int *ele1, int *ele2);
 
+/**
+ * 数组元素交换位置
+ * @param { int ref } 元素指针1
+ * @param { int ref } 元素指针2
+ */
+void swap (int *ele1, int *ele2) {
+  int temp;
+  temp = *ele1;
+  *ele1 = *ele2;
+  *ele2 = temp;
+} 
