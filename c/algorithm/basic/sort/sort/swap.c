bed9c58a164c8088020b14a99a593ad14b4a6cab
mulberry <wowcxy2008@126.com>
2018-01-31 21:16:58 +0800

    Create swap.c

---
+++ c/algorithm/basic/sort/sort/swap.c
@@ -0,0 +1,16 @@
+/**
+ * 交换
+ * @author mulberry
+ */
+
+/**
+ * 数组元素交换位置
+ * @param { int ref } 元素指针1
+ * @param { int ref } 元素指针2
+ */
+void swap (int &ele1, int &ele2) {
+  int temp;
+  temp = ele1;
+  ele1 = ele2;
+  ele2 = ele1;
+} 
