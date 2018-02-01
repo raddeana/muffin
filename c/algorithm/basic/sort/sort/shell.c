79e4d8e69b938419de9e1ba65a7503caf829e8a7
mulberry <wowcxy2008@126.com>
2018-02-01 15:59:07 +0800

    Update shell.c

--- c/algorithm/basic/sort/sort/shell.c
+++ c/algorithm/basic/sort/sort/shell.c
@@ -1,12 +1,41 @@
+/**
+ * 希尔排序
+ * @author mulberry
+ */
+
 #include <stdio.h>
 
-int main () {
-  int a, b;
-  
-  a = 56;
-  b = 45;
-  printf("sum is:%d\n",a+b);
-  printf("test exec!\n");
+void shell (int arr[]);
 
-  return 0;
+/**
+ * 希尔排序
+ * @param {int[]} 一个可排序数组
+ */
+void shell (int arr[]) {
+  int h = 0;
+  int len = sizeof(arr);
+  
+  while (h <= len)
+  {
+    h = 3 * h + 1;
+  }
+  
+  while (h >= 1)
+  {
+    for (int i = h; i < n; i++)
+    {
+      int j = i - h;
+      int get = arr[i];
+      
+      while (j >= 0 && A[j] > get)
+      {
+        arr[j + h] = arr[j];
+        j = j - h;
+      }
+      
+      arr[j + h] = get;
+    }
+    
+    h = (h - 1) / 3;
+  }
 }
