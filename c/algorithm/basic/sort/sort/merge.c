0b6aa06eb57f1247de2b8966986202b5ce45005a
mulberry <wowcxy2008@126.com>
2018-02-01 17:03:47 +0800

    Update merge.c

--- c/algorithm/basic/sort/sort/merge.c
+++ c/algorithm/basic/sort/sort/merge.c
@@ -1,12 +1,85 @@
 #include <stdio.h>
 
-int main () {
-  int a, b;
-  
-  a = 56;
-  b = 45;
-  printf("sum is:%d\n",a+b);
-  printf("test exec!\n");
+/**
+ * 克隆数组
+ * @param {int[]} 源数组
+ * @param {int[]} 目标数组
+ * @param {int[]} 克隆长度
+ * @param {int[]} 起始位置
+ */
+void copyArray(int source[], int dest[], int len, int first)    
+{    
+   int i;    
+   int j = first;
 
-  return 0;
+   for(i = 0; i < len; i ++)
+   {    
+      dest[j] = source[i];
+      j++;
+   }     
+}
+
+/**
+ * 克隆数组
+ * @param {int[]} 源数组
+ * @param {int} 混合左侧起始位置
+ * @param {int} 混合右侧起始位置
+ */
+void merge(int arr[], int left, int right)    
+{    
+   int begin1 = left;    
+   int mid = (left + right) / 2 ;    
+   int begin2 = mid + 1;    
+   int k = 0;    
+   int newArrayLen = right - left + 1;    
+   int *b = (int*)malloc(newArrayLen * sizeof(int));
+
+   while(begin1 <= mid && begin2 <= right)
+   {
+      if (arr[begin1] <= arr[begin2]) {    
+         b[k ++] = arr[begin1 ++];    
+      } else {    
+         b[k ++] = arr[begin2 ++];
+      }
+   }
+
+   while(begin1 <= mid) {
+      b[k ++] = arr[begin1 ++];
+   }
+
+   while(begin2 <= right) {
+      b[k ++] = arr[begin2 ++];
+   }
+
+   copyArray(b, arr, newArrayLen, left);
+   free(b);
+}
+
+/**
+ * 归并排序
+ * @param {int[]} 源数组
+ * @param {int} 排序位置左起
+ * @param {int} 排序位置最右侧
+ */
+void mergeSort(int arr[], int left, int right)    
+{    
+   int i;
+
+   if (left < right)    
+   {
+      i = (left + right) / 2;
+      mergeSort(arr, left, i);
+      mergeSort(arr, i + 1, right);
+      merge(arr, left, right);
+   }
+}
+
+/**
+ * 归并排序
+ * @param {int[]} 源数组
+ */
+void MergeSort(int arr[])
+{
+   int len = sizeof(arr);
+   mergeSort(arr, 0, len - 1);  
 }
