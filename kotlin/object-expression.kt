ebe375d4f6331e5988a64858b2c0580bf22068d4
mulberry <wowcxy2008@126.com>
2018-02-02 21:52:01 +0800

    Create object-expression.kt

---
+++ kotlin/object-expression.kt
@@ -0,0 +1,8 @@
+window.addMouseListener(object : MouseAdapter() {
+    override fun mouseClicked(e: MouseEvent) {
+        // ...
+    }
+    override fun mouseEntered(e: MouseEvent) {
+        // ...
+    }
+})
