cfc44173c650e97e35570680d1f813acf37d4363
mulberry <wowcxy2008@126.com>
2018-02-02 17:58:25 +0800

    Create loop.kt

---
+++ kotlin/loop.kt
@@ -0,0 +1,18 @@
+// 键循环
+for (i in array.indices) {
+  print(array[i])
+}
+
+// 值循环
+for (item in items) {
+  println(item)
+}
+
+// while 循环
+while( 布尔表达式 ) {
+  // 循环内容
+}
+
+do {
+  // 代码语句
+} while (布尔表达式);
