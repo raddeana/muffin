a8b388fe4c0e32197895f05a9462c643bb5a0930
mulberry <wowcxy2008@126.com>
2018-02-07 15:58:58 +0800

    Update singleton.kt

--- design_pattern/kotlin/singleton.kt
+++ design_pattern/kotlin/singleton.kt
@@ -1,1 +1,12 @@
+class Singleton {
+    private var singletonInstance : Singleton ?= null
+
+    @Synchronized fun getInstance : Singleton () {
+      if (instance == null) {
+        instance = Singleton ();
+      }
+      
+      return instance;
+    }
+}
 
