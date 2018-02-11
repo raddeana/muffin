de7e3b5feb51819c5d575598551413d55349936c
mulberry <wowcxy2008@126.com>
2018-02-11 14:06:46 +0800

    Update factory.kt

--- design_pattern/kotlin/factory.kt
+++ design_pattern/kotlin/factory.kt
@@ -1,23 +1,38 @@
+/**
+ * 抽象基类，形状
+ */
 abstract class Shape {
   abstract fun print ()
 }
 
+/**
+ * 圆形
+ */
 class Circle : Shape () {
   override fun print () {
     println("print of Circle")
   }
 }
 
+/**
+ * 三角形
+ */
 class Triangle : Shape() {
   override fun print () {
     println("print of ConcreteProductB")
   }
 }
 
+/**
+ * 抽象类工厂
+ */
 abstract class Factory {
   abstract fun factoryMethod(): Shape
 }
 
+/**
+ * 圆形工厂
+ */
 class CircleFactory : Factory () {
   override fun factoryMethod (): Circle {
     println("create Circle")
@@ -25,6 +40,9 @@ class CircleFactory : Factory () {
   }
 }
 
+/**
+ * 三角形工厂
+ */
 class TriangleFactory : Factory () {
   override fun factoryMethod (): Triangle {
     println("create Triangle")
