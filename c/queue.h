3de418ee63e86f95e863722175150e9ea823fba5
Steudnera Taro <wowcxy2008@126.com>
2018-04-20 21:27:31 +0800

    Update queue.h

--- c/queue.h
+++ c/queue.h
@@ -19,50 +19,50 @@ typedef struct queue {
     datatype queue_array[MAX_QUEUE_SIZE];
     int front;
     int rear;
-} sp_queue;
+} spQueue;
 
 /* 静态顺序链的接口定义 */
 /* 静态链的初始化 */
-sp_queue queueInit();
+spQueue queueInit();
 
 /**
  * 判断队列是否为空,若为空
  * 返回true
  * 否则返回false
  */
-int queueEmpty(sp_queue q);
+int queueEmpty(spQueue *q);
 
 /**
  * 插入元素e为队q的队尾新元素 
  * 插入成功返回true，队满返回false
  */
-int enqueue(sp_queue *q, datatype e);
+int enqueue(spQueue *q, datatype e);
 
 
 /** 
  * 队头元素出队
  * 用e返回出队元素，并返回true，若队空返回false
  */
-int dequeue(sp_queue *q, datatype *e);
+int dequeue(spQueue *q, datatype *e);
 
 /** 
  * 清空队 
  */
-void clearqueue(sp_queue *q);
+void clearQueue(spQueue *q);
 
 /** 
  * 获得队头元素
  * 队列非空，用e返回队头元素，并返回true，否则返回false
  */
-int getfront(sp_queue, datatype *e);
+int getfront(spQueue *q, datatype *e);
 
 /** 
  * 获得length
  */
-int queueLen(sp_queue q);
+int queueLen(spQueue *q);
 
 /**
  * 遍历队 
  */
-void queueTraverse(sp_queue q, void(*visit)(sp_queue q));
-void visit(sp_queue s);
+void queueTraverse(spQueue *q, void(*visit)(spQueue *q));
+void visit(spQueue *q);
