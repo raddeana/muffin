f9926cfeac55f4601320bd0d102e96004d9900d5
Steudnera C. Koch <wowcxy2008@126.com>
2018-06-20 11:25:04 +0800

    Update server.c

--- languages/c/socket/server.c
+++ languages/c/socket/server.c
@@ -7,16 +7,16 @@
 #include<sys/socket.h>
 #include<sys/wait.h>
 
-#define PORT 1500     // 端口号
+#define PORT 1500     /* 端口号 */
 #define BACKLOG 5     /* 最大监听数 */
 
 int main () {
-    int sockfd,new_fd;                  /* socket句柄和建立连接后的句柄 */
-    struct sockaddr_in my_addr;         /* 本方地址信息结构体; 下面有具体的属性赋值 */
-    struct sockaddr_in their_addr;      /* 对方地址信息 */
+    int sockfd, new_fd;                         /* socket句柄和建立连接后的句柄 */
+    struct sockaddr_in my_addr;                 /* 本方地址信息结构体; 下面有具体的属性赋值 */
+    struct sockaddr_in their_addr;              /* 对方地址信息 */
     int sin_size;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);   //  建立socket 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);   /* 建立socket */ 
     
     if (sockfd==-1) {
         printf("socket failed:%d", errno);
@@ -28,18 +28,20 @@ int main () {
     my_addr.sin_addr.s_addr = htonl(INADDR_ANY);    /* IP，括号内容表示本机IP */
     bzero(&(my_addr.sin_zero), 8);                  /* 将其他属性置0 */
     
-    if (bind(sockfd,(struct sockaddr*) &my_addr, sizeof(struct sockaddr))<0) {  // 绑定地址结构体和socket
+    /* 绑定地址结构体和socket */
+    if (bind(sockfd, (struct sockaddr*) &my_addr, sizeof(struct sockaddr)) < 0) {
         printf("bind error");
         return -1;
     }
     
-    listen(sockfd, BACKLOG);                                                // 开启监听，第二个参数是最大监听数
+    /* 开启监听，第二个参数是最大监听数 */
+    listen(sockfd, BACKLOG);
     
     while (1) {
         sin_size = sizeof(struct sockaddr_in);
         new_fd = accept(sockfd, (struct sockaddr*)&their_addr, &sin_size);  // 在这里阻塞知道接收到消息，参数分别是socket句柄，接收到的地址信息以及大小 
         
-        if (new_fd==-1) {
+        if (new_fd == -1) {
             printf("receive failed");
         } else {
             printf("receive success");
