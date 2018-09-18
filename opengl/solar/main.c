c35473fbfec22e6c13db7f42a11f120ae798afa7
Discontinuity <wowcxy2008@126.com>
2018-09-18 11:35:42 +0800

    Update and rename display.c to main.c

--- opengl/solar/main.c
+++ opengl/solar/main.c
@@ -3,7 +3,11 @@
 #include <time.h>
 
 static int day = 200;
- 
+
+/**
+ * 获取当前FPS
+ * @return
+ */
 double getFPS () {
 	static int count;
 	static double save;
@@ -24,14 +28,18 @@ double getFPS () {
 	
   return save;
 }
- 
+
+/**
+ * 获取当前FPS
+ * @return
+ */
 void display () {
 	// 打印FPS
 	double FPS = getFPS();
 	printf("FPS = %f\n", FPS);
  
 	glEnable(GL_DEPTH_TEST);                              // 开启更新深度缓冲区的功能，被挡住的像素不绘制
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);   // 清除颜色缓冲|深度缓冲
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);   // 清除颜色缓冲 | 深度缓冲
  
 	glMatrixMode(GL_PROJECTION);                // 设置当前矩阵模式：对投影矩阵应用随后的矩阵操作
 	glLoadIdentity();                           // 将之前矩阵变换导致变化过的栈顶矩阵重新归位，置为单位矩阵,即之前的矩阵变换带来的影响到此为止
@@ -41,7 +49,7 @@ void display () {
 	glLoadIdentity();                                   // 将之前矩阵变换导致变化过的栈顶矩阵重新归位，置为单位矩阵,即之前的矩阵变换带来的影响到此为止
 	gluLookAt(0, -200000, 200000, 0, 0, 0, 0, 0, 1);    // 设置视点
  
-	//sun
+	// sun
 	glColor3f(1, 0, 0);                 // 设置绘制颜色为红色
 	glutSolidSphere(69600, 20, 20);     // 渲染一个球体
  
@@ -51,7 +59,7 @@ void display () {
 	glTranslatef(150000, 0, 0);         // 平移
 	glutSolidSphere(15945, 20, 20);     // 渲染一个球体
  
-	//moon
+	// moon
 	glColor3f(1, 1, 0);
 	glRotatef(day / 30.0 * 360 - day, 0, 0, -1);    // 旋转
 	glTranslatef(38000, 0, 0);                      // 平移
@@ -60,21 +68,29 @@ void display () {
 	glFlush();                          // 清空缓冲区，立即执行绘制命令  
 	glutSwapBuffers();                  // 双缓冲时交换两个缓冲区指针
 }
- 
+
+/**
+ * 空闲
+ * @return
+ */
 void idle () {
 	++day;
   
-	if (day > 360){
+	if (day > 360) {
 		day = 0;
 	}
   
 	display();                          // 调用display方法进行强制刷新
 }
- 
+
+/**
+ * 主函数
+ * @return
+ */
 int main (int argc, char **argv) {
-	glutInit(&argc, argv);// 初始化GLUT
+	glutInit(&argc, argv);													// 初始化GLUT
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);    // 显示显示模式(RGB颜色|双缓冲)
-	glutCreateWindow("earth,moon,sun");             // 设置窗口标题
+	glutCreateWindow("earth, moon, sun");             // 设置窗口标题
 	glutInitWindowSize(400, 400);       // 设置窗口大小
 	glutDisplayFunc(display);           // 设置绘制函数
 	glutIdleFunc(&idle);
