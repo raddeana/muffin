a01519c0192acccb8b9a8ece205adb68e39a4492
Discontinuity <wowcxy2008@126.com>
2018-09-21 10:48:30 +0800

    Update ellipse.cpp

--- opengl/ellipse.cpp
+++ opengl/ellipse.cpp
@@ -42,11 +42,11 @@ void display () {
 	glFlush();
 }
 
-int main (int argc,char **argv) {
-	glutInit(&argc,argv);
+int main (int argc, char **argv) {
+	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(500,500);
-	glutInitWindowPosition(0,0);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
 	glutCreateWindow("hello");
 	
   init();
