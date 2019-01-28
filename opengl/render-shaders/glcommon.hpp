53501aff62c4d97fd07fea71958af2e59c7da80a
Philip <wowcxy2008@126.com>
2019-01-28 19:34:03 +0800

    Create glcommon.hpp

---
+++ opengl/render-shaders/glcommon.hpp
@@ -0,0 +1,11 @@
+#ifndef HJ_GLCOMMON_HPP_
+#define HJ_GLCOMMON_HPP_
+
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+GLuint createGLProgram(const char *vertPath, const char *fragPath);
+
+#endif /* HJ_GLCOMMON_HPP_ */
