8f64efd4660393f7879c6aee8584ba8b0f6904ab
Philip <wowcxy2008@126.com>
2019-07-05 10:44:54 +0800

    Update main.cpp

--- opengl/render-shaders/main.cpp
+++ opengl/render-shaders/main.cpp
@@ -135,8 +135,3 @@ int main (int argc, char *argv[]) {
 	glutMainLoop();
 	return 0;
 }
---------------------- 
-作者：Drunkenman_ 
-来源：CSDN 
-原文：https://blog.csdn.net/huanghuangjin/article/details/84556905 
-版权声明：本文为博主原创文章，转载请附上博文链接！
